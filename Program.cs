76fcc34c46903cf257e212e9f07a8d73cf473806
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-22 10:58:38 -0300

    Congratulations!

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write ("Olá, usuário! Digite a sua senha: ");
+string senha  = Console.ReadLine()!;
+
+if (senha == "1234abcd")
+{
+    Console.WriteLine("Acesso permitido");
+}
+
+else 
+{
+    Console.WriteLine("acesso negado");
+}
+
